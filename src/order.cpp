d72807c8d2dd5954490d01fa2a5ba7c46bf63c32
Dillon Huff <dillonhuff@gmail.com>
2017-05-30 13:15:06 -0700

    Basic order generation is passing a test

--- src/order.cpp
+++ src/order.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 namespace lqe {
 
-  std::vector<order> update_orders(const unsigned i, const order& ord) {
+  std::vector<order> update_orders(const unsigned fresh, const order& ord) {
     vector<order> ords;
 
     // Add one new order for each equivalence class in the original order, where
@@ -26,6 +26,26 @@ namespace lqe {
 
       ords.push_back(fresh);
 
+    }
+
+    // Create order where the new element is the largest element
+    order i_is_max = ord;
+    i_is_max.push_back({fresh});
+    ords.push_back(i_is_max);
+
+    // Create groups where the new element is in between or smallest
+    for (int i = 0; i < ord.size(); i++) {
+      order fresh_ord;
+
+      for (int j = 0; j < i; j++) {
+	fresh_ord.push_back(ord[j]);
+      }
+      fresh_ord.push_back({fresh});
+      for (int j = i + 1; j < ord.size(); j++) {
+	fresh_ord.push_back(ord[j]);
+      }
+
+      ords.push_back(fresh_ord);
     }
 
     return ords;
